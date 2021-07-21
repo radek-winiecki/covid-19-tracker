daaaf6aeff884384b0d1499817796cc4fc491f37
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-21 11:38:15 +0200

    Select all elements

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+// select all elements
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
