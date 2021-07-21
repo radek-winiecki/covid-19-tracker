5b46faccb35d84cfac0fb565594f65be96159f2a
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-21 12:49:48 +0200

    Implement the Chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -62,7 +62,7 @@ fetchData(user_country);
 // update UI function
 function updateUI() {
     updateStats();
-    // axesLinearChart();
+    axesLinearChart();
 }
 
 function updateStats() {
@@ -79,4 +79,34 @@ function updateStats() {
 
     deaths_element.innerHTML = last_entry.total_deaths;
     new_deaths_element.innerHTML = `+${last_entry.new_deaths || 0}`;
+}
+
+// update Chart
+let my_chart;
+
+function axesLinearChart() {
+    if (my_chart) {
+        my_chart.destroy();
+    }
+
+    my_chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            datasets: [{
+                label: 'First dataset',
+                data: [0, 20, 40, 50],
+            }],
+            labels: ['January', 'February', 'March', 'April'],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 50,
+                        suggestedMax: 100
+                    }
+                }]
+            }
+        },
+    });
 }
